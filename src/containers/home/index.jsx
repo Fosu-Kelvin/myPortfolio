26641e5118af370497c05d2c399eb3f5c82fab89
Kelvin Fosu <kelfosu19@gmail.com>
2025-03-25 19:27:46 +0000

    updated portfolio

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 <div className="container1">
       <p>Hi,I'm Kelvin</p>
       <h1>Front End <span>Developer</span></h1>
-      <p>I have a strong background in creating visually appealing and user-friendly web experiences.I am motivated to find a role where i can challenge myself and provide value to website users.I am exicited to bring my knowledge and experience to a team and contribute to a company's success.</p>
+      <p className='para-2'>I have a strong background in creating visually appealing and user-friendly web experiences.I am motivated to find a role where i can challenge myself and provide value to website users.I am exicited to bring my knowledge and experience to a team and contribute to a company's success.</p>
       <div className='home-buttons'>
       <Link to="/contact"><button>Get in Touch</button></Link>
       <a href={pdf} download="Kelvin CV.pdf">
--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -3,17 +3,19 @@ import photo1 from './images/photo1.avif'
 import photo2 from './images/photo2.jpeg'
 import photo3 from './images/photo3.jpg'
 import photo4 from './images/photo4.jpg'
+import photo5 from './images/photo5.jpg'
 import { FaShare } from 'react-icons/fa'
 
 const Portfolio = () => {
-  const pics = [photo1,photo2,photo3,photo4]
-  const projectNames  = ["Weather App","To-do list App","Calculator","Stop Watch"]
+  const pics = [photo1,photo2,photo3,photo4,photo5]
+  const projectNames  = ["Weather App","To-do list App","Calculator","Stop Watch","trading site"]
 
   const links = [
     "https://github.com/Fosu-Kelvin/Weather-app1",
     "https://github.com/Fosu-Kelvin/To-do-list-app",
     "https://github.com/Fosu-Kelvin/calculator-app",
     "https://github.com/Fosu-Kelvin/stop-watch-app",
+    "https://Fosu-Kelvin.github.io/financeEdge"
   ];
   return (
     <section className="portfolio-container">
